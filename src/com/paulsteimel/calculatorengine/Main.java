14c640365602740e901f368566ddbae8fb8b3ad0
Paul Steimel <paulsteimel@gmail.com>
2018-05-28 18:28:57 -0500

    Adding the basic operations

--- src/com/paulsteimel/calculatorengine/Main.java
+++ src/com/paulsteimel/calculatorengine/Main.java
@@ -3,6 +3,27 @@ package com.paulsteimel.calculatorengine;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    double val1 = 100.0d;
+	    double val2 = 50.0d;
+	    double result;
+	    char opCode = 'd';
+
+	    if (opCode == 'a')
+	        result = val1 + val2;
+	    else if (opCode == 's')
+	        result = val1 - val2;
+	    else if (opCode == 'd')
+	    	result = val2 != 0.0d ? val1 / val2 : 0.0d;
+	    else if (opCode == 'm')
+	        result = val1 * val2;
+	    else {
+			System.out.println("Error - invalid opCode");
+			result = 0.0d;
+		}
+
+        System.out.println(result);
+
+
+
     }
 }
