efb48a4e11835a339bc47262c861188c58c896ac
Paul Steimel <paulsteimel@gmail.com>
2018-05-30 16:57:20 -0500

    Method overloads added.

--- src/com/paulsteimel/calculatorengine/MathEquation.java
+++ src/com/paulsteimel/calculatorengine/MathEquation.java
@@ -30,6 +30,23 @@ public class MathEquation {
 
     }
 
+    public void execute(double leftVal, double rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+    }
+
+    public void execute(int leftVal, int rightVal){
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+
+        execute();
+
+        result = (int)result;
+    }
+
+
     public void execute() {
         switch (opCode) {
             case 'a':
